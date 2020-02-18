a5abf8760246a4c37d3b564df16241ab97ec0989
Yuri Serka <yserka@hotmail.com>
2020-02-18 13:00:47 -0300

    retirando informações inuteis

--- config.ts
+++ config.ts
@@ -12,13 +12,3 @@ export const googleAccount = {
   email: process.env.GOOGLE_EMAIL || 'none',
   senha: process.env.GOOGLE_SENHA || 'none',
 };
-
-export const xenAccount = {
-  username: process.env.XEN_ORCHESTRA_USERNAME || 'none',
-  senha: process.env.XEN_ORCHESTRA_SENHA || 'none',
-};
-
-export const ftpAccount = {
-  username: process.env.VM_FTP_USERNAME || 'none',
-  senha: process.env.VM_FTP_SENHA || 'none',
-};
